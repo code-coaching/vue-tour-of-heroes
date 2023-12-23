69501d8b48df06caece7505eda79692ce700b9aa
Bart Duisters <bartduisters@bartduisters.com>
2023-12-23 18:26:01 +0100

    feat: dashboard view

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,3 +1,49 @@
 <template>
-  <div>Dashboard works!</div>
+  <div class="title">Top Heroes</div>
+
+  <div class="top-heroes">
+    <div class="top-hero">Narco</div>
+    <div class="top-hero">Bombasto</div>
+    <div class="top-hero">Celeritas</div>
+    <div class="top-hero">Magneta</div>
+  </div>
 </template>
+
+<style scoped>
+.title {
+  font-size: 1.5rem;
+  color: grey;
+  font-weight: bold;
+
+  display: flex;
+  justify-content: center;
+  margin-bottom: 1rem;
+}
+
+.top-heroes {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 2rem;
+}
+
+.top-hero {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: #5f7d8c;
+  color: white;
+
+  height: 8rem;
+  width: 10rem;
+  font-weight: 600;
+  font-size: 1.1rem;
+}
+
+.top-hero:hover {
+  background-color: #eeeeee;
+  color: #5f7d8c;
+  cursor: pointer;
+}
+</style>
