15f83d5192ffeaa2b2454838b1e511089b1a4d8b
Bart Duisters <bartduisters@bartduisters.com>
2023-12-23 18:11:52 +0100

    styling layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,9 +5,46 @@ const router = useRouter();
 </script>
 
 <template>
-  <div>Tour of Heroes</div>
-  <button @click="router.push({ name: 'dashboard' })">Dashboard</button>
-  <button @click="router.push({ name: 'hero-list' })">Heroes</button>
+  <div class="layout-container">
+    <div class="title">Tour of Heroes</div>
 
-  <RouterView />
+    <div class="button-container">
+      <button @click="router.push({ name: 'dashboard' })">Dashboard</button>
+      <button @click="router.push({ name: 'hero-list' })">Heroes</button>
+    </div>
+
+    <RouterView />
+  </div>
 </template>
+
+<style scoped>
+.title {
+  font-size: 1.5rem;
+  color: grey;
+  font-weight: bold;
+}
+
+.layout-container {
+  margin: 2rem;
+}
+
+.button-container {
+  display: flex;
+  gap: 0.25rem;
+}
+
+button {
+  background-color: #eeeeee;
+  border-radius: 0.25rem;
+  font-weight: 500;
+  border: none;
+  padding: 0.25rem 0.5rem;
+  color: #567868;
+}
+
+button:hover {
+  background-color: #e6e6e6;
+  color: #0096e8;
+  cursor: pointer;
+}
+</style>
