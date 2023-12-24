140e63b2c8d7e927f0b52d4afc2cb635941e7ad9
Bart Duisters <bartduisters@bartduisters.com>
2023-12-24 14:18:20 +0100

    feat: hardcoded details

--- src/views/HeroDetailsView.vue
+++ src/views/HeroDetailsView.vue
@@ -1,3 +1,32 @@
+<script setup lang="ts">
+import StyledButton from '@/components/StyledButton.vue';
+import { ref, type Ref } from 'vue';
+
+interface Hero {
+  number: number;
+  name: string;
+}
+
+const hero: Ref<Hero | null> = ref({ number: 15, name: 'Magneta' });
+</script>
+
 <template>
-  <div>Hero details works!</div>
+  <template v-if="hero">
+    <div class="title">{{ hero.name }} details!</div>
+
+    <div>id: {{ hero.number }}</div>
+    <div>name: <input :value="hero.name" /></div>
+
+    <StyledButton>Back</StyledButton>
+  </template>
 </template>
+
+<style scoped>
+.title {
+  font-size: 1.5rem;
+  color: grey;
+  font-weight: bold;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+}
+</style>
