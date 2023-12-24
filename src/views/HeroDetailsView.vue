716349e0287e84a1650b239bd91b04ba102e63cd
Bart Duisters <bartduisters@bartduisters.com>
2023-12-24 14:39:23 +0100

    feat: hero url parameter

--- src/views/HeroDetailsView.vue
+++ src/views/HeroDetailsView.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
 import StyledButton from '@/components/StyledButton.vue';
 import type { Hero } from '@/components/models';
-import { ref, type Ref } from 'vue';
+import { onMounted, ref, type Ref } from 'vue';
+import { useRoute } from 'vue-router';
 
-const hero: Ref<Hero | null> = ref({ number: 15, name: 'Magneta' });
+const route = useRoute();
+
+const hero: Ref<Hero | null> = ref(null);
+const heroes = [
+  { number: 11, name: 'Mr. Nice' },
+  { number: 12, name: 'Narco' },
+  { number: 13, name: 'Bombasto' },
+  { number: 14, name: 'Celeritas' },
+  { number: 15, name: 'Magneta' },
+  { number: 16, name: 'RubberMan' },
+  { number: 17, name: 'Dynama' },
+  { number: 18, name: 'Dr IQ' },
+  { number: 19, name: 'Magma' },
+  { number: 20, name: 'Tornado' }
+];
+
+onMounted(() => {
+  const heroId = Number(route.params.id);
+  hero.value = heroes.find((hero) => hero.number === heroId) ?? null;
+});
 </script>
 
 <template>
