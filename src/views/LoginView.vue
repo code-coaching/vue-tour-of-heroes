932eae648d12a929ff8a2f8caddd6a0aff061777
Bart Duisters <bartduisters@bartduisters.com>
2023-12-28 22:21:38 +0100

    refactor: style login page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import StyledButton from '@/components/StyledButton.vue';
 import { useAuth } from '@/services/auth.service';
 import { ref } from 'vue';
 
@@ -12,11 +13,31 @@ const authenticate = () => {
     email.value = '';
     password.value = '';
   });
-}
+};
 </script>
 
 <template>
-  <input type="email" v-model="email" />
-  <input type="password" v-model="password" />
-  <button @click="authenticate()">Login</button>
+  <div class="login-container">
+    <div class="login-fields">
+      <input type="email" v-model="email" />
+      <input type="password" v-model="password" />
+    </div>
+    <StyledButton @click="authenticate()">Login</StyledButton>
+  </div>
 </template>
+
+<style scoped>
+.login-container {
+  margin-top: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  max-width: 20rem;
+}
+
+.login-fields {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+}
+</style>
